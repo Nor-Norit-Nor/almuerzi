836671365a9f243e13161c7206281f7a31d9ec8a
ngc <nora.gonzalo@gmail.com>
2021-01-11 19:26:50 +0100

    ADD listItem + custom hook fetch

--- screens/Meals.js
+++ screens/Meals.js
@@ -1,10 +1,44 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, StyleSheet, FlatList, Text} from 'react-native'
+import ListItem from '../components/ListItem'
+import useFetch from '../hooks/useFetch'
+import usefetch from '../hooks/useFetch'
+const styles= StyleSheet.create({
+	container:{
+		flex:1,
+		backgroundColor:'#fff',
+		alignItems:'flex-start',
+		justifyContent:'flex-start'
+	},
+	list:{
+		alignSelf:'stretch'
+	}
+})
+// const data = [
+// 	{ _id:'1', name:'churrasco', desc:'plato títpico'},
+// ]
+const Meals = ({navigation})=>{
+	const {loading, data : meals} = usefetch('https://serverless.nor-norit-nor.vercel.app/api/meals')
 
-export default ()=>{
 	return (
-		<View>
-			<Text>soy meals</Text>
+		<View style={styles.container}>
+			{loading ? <Text>Cargando...</Text> : 
+			<FlatList 
+			style={styles.list}
+			data={meals}
+			keyExtractor={x => x._id}
+			renderItem={({item})=>
+				<ListItem 
+				onPress={()=>navigation.navigate('Modal', { _id: item._id })}
+				name={item.name}
+				/>
+			}
+		/>	}
 		</View>
 	)
 }
+Meals.navigationOptions = ({
+	title: 'Comidas disponibles',
+})
+
+export default Meals
